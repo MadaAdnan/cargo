0fa3c78299df061f721cb3a36e960b022fffec2a
adnan <mh.shamey@gmail.com>
2024-11-17 22:52:40 +0300

    add excel

--- app/Filament/Admin/Resources/OrderResource/Pages/CreateOrder.php
+++ app/Filament/Admin/Resources/OrderResource/Pages/CreateOrder.php
@@ -25,9 +25,10 @@ class CreateOrder extends CreateRecord
 
         $city_source=City::find($data['city_source_id']);
         $city_target=City::find($data['city_target_id']);
-        $target=User::where('level',LevelUserEnum::BRANCH->value)->where('branch_id',$data['branch_target_id'] )->first();
+
         $data['branch_source_id'] =$city_source->branch_id;
         $data['branch_target_id'] =$city_target->branch_id;
+        $target=User::where('level',LevelUserEnum::BRANCH->value)->where('branch_id',$data['branch_target_id'] )->first();
         $data['given_id']=$target?->id;
         $data['code'] = "AWB" . now()->format('YmdHis'); // الطابع الزمني بتنسيق قصير
         $data['shipping_date'] = now()->format('Y-m-d');
