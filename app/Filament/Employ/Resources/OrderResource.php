3bcb5497ad682b85e5edf2de21bacce9c8220255
adnan <mh.shamey@gmail.com>
2024-11-20 18:41:12 +0300

    add set staff for returned action

--- app/Filament/Employ/Resources/OrderResource.php
+++ app/Filament/Employ/Resources/OrderResource.php
@@ -379,7 +379,7 @@ class OrderResource extends Resource
                             Notification::make('success')->title('نجاح العملية')->body('تم تغيير حالة الطلب')->success()->send();
                         } catch (\Exception | Error $e) {
                             DB::rollBack();
-                            Notification::make('error')->title('فشل العملية')->body($e->getLine())->danger()->send();
+                            Notification::make('error')->title('فشل العملية')->body($e->getMessage())->danger()->send();
                         }
                     })->label('تأكيد تسليم المرتجع')->color('danger')
                     ->requiresConfirmation()
