d8fa29701a6868d4aeaad3700f971c1da3b85236
adnan <mh.shamey@gmail.com>
2025-01-02 20:32:04 +0300

    add color to order

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -17,16 +17,21 @@ class OrderObserver
 
     public function creating(Order $order): void
     {
-        if($order->pick_id==null){
+        if ($order->pick_id == null) {
             $order->status = OrderStatusEnum::PENDING;
-        }else{
+        } else {
             $order->status = OrderStatusEnum::PICK;
         }
-        $order->created_by=auth()->id();
-        $order->given_id=User::where([
-            'level'=>LevelUserEnum::BRANCH->value,
-            'branch_id'=>$order->branch_target_id
-            ])->first()?->id;
+        $order->created_by = auth()->id();
+        $given_id = User::where([
+            'level' => LevelUserEnum::BRANCH->value,
+            'branch_id' => $order->branch_target_id
+        ])->first()?->id;
+        $order->given_id = $given_id;
+        if ($given_id != null) {
+            $order->status = OrderStatusEnum::TRANSFER;
+        }
+
 
     }
 
@@ -46,8 +51,8 @@ class OrderObserver
             } catch (\Exception | \Error $e) {
                 \DB::rollBack();
                 info("Error Observe in created function");
-                info('Message:'.$e->getMessage());
-                info('File:'.$e->getFile(). ' Line:'.$e->getLine());
+                info('Message:' . $e->getMessage());
+                info('File:' . $e->getFile() . ' Line:' . $e->getLine());
             }
 
         }
@@ -61,11 +66,9 @@ class OrderObserver
     {
 
 
-
-        if ($order->status->value == OrderStatusEnum::CANCELED->value || $order->status->value == OrderStatusEnum::CONFIRM_RETURNED->value ) {
+        if ($order->status->value == OrderStatusEnum::CANCELED->value || $order->status->value == OrderStatusEnum::CONFIRM_RETURNED->value) {
             $order->balances()->delete();
         }
-
 
 
     }
