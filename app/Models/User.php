d2f471ef46fd36c697cd8ea313c36185614f3244
adnan <mh.shamey@gmail.com>
2025-01-18 10:54:27 +0300

    add livewire

--- app/Models/User.php
+++ app/Models/User.php
@@ -85,6 +85,7 @@ class User extends Authenticatable implements HasMedia, FilamentUser, HasAvatar
         'level' => LevelUserEnum::class,
         'status' => ActivateStatusEnum::class,
         'job' => JobUserEnum::class,
+        'location' => 'array'
 
     ];
 
