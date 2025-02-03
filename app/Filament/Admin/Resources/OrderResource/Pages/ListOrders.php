d23abf64b437e32a8f601f0a658927f97568f196
Ab00dSte <abdullah.it.ste@gmail.com>
2025-02-03 19:55:41 +0300

    fix: update export action URL to use table filters directly and remove unused method for filter retrieval

--- app/Filament/Admin/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Admin/Resources/OrderResource/Pages/ListOrders.php
@@ -22,14 +22,9 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
             Actions\Action::make('fast-order')->url('/admin/orders/fast-order')->label('شحنة سريعة'),
             Actions\Action::make('export')
-                ->url(fn() => route('export-order', ['filters' => $this->getTableFiltersFromLivewire()]), true)
+                ->url(fn() => route('export-order', ['filters' => $this->tableFilters ?? []]), true)
                 ->label('تصدير إلى Excel'),
         ];
-    }
-
-    protected function getTableFiltersFromLivewire(): array
-    {
-        return $this->tableFilters ?? [];
     }
 
     protected function getTableQuery(): ?Builder
