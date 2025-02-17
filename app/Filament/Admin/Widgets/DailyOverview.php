ddb1eea7ad2a01d8d4ebbc063dffbf8654a09181
adnan <mh.shamey@gmail.com>
2025-02-17 12:28:54 +0300

    add abdullah new

---
+++ app/Filament/Admin/Widgets/DailyOverview.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Admin\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class DailyOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $ordersNum=Order::whereDate('created_at',now());
+        return [
+            Stat::make('الشحنات المنشأة اليوم', $ordersNum->count()),
+            Stat::make('إجمالي قيمة الشحنات USD', $ordersNum->sum('price')),
+            Stat::make('إجمالي قيمة الشحنات TRY', $ordersNum->sum('price_tr')),
+            Stat::make('إجمالي أجور الشحنات USD  على المرسل', $ordersNum->where('orders.far_sender',1)->sum('far')),
+            Stat::make('إجمالي أجور الشحنات USD  على المستلم', $ordersNum->where('orders.far_sender',0)->sum('far')),
+            Stat::make('إجمالي أجور الشحنات TRY  على المرسل', $ordersNum->where('orders.far_sender',1)->sum('far_tr')),
+            Stat::make('إجمالي أجور الشحنات TRY  على المستلم', $ordersNum->where('orders.far_sender',0)->sum('far_tr')),
+        ];
+    }
+}
