a27a69ba0f2c1da9ce203b5c6d946cd478f0e7db
adnan <mh.shamey@gmail.com>
2025-02-17 12:32:46 +0300

    add abdullah new

--- app/Filament/Admin/Widgets/DailyOverview.php
+++ app/Filament/Admin/Widgets/DailyOverview.php
@@ -16,10 +16,10 @@ class DailyOverview extends BaseWidget
             Stat::make('الشحنات المنشأة اليوم', $ordersNum->count()),
             Stat::make('إجمالي قيمة الشحنات USD', $ordersNum->sum('price')),
             Stat::make('إجمالي قيمة الشحنات TRY', $ordersNum->sum('price_tr')),
-            Stat::make('إجمالي أجور الشحنات USD  على المرسل', $ordersNum->where('orders.far_sender',1)->sum('far')),
-            Stat::make('إجمالي أجور الشحنات USD  على المستلم', $ordersNum->where('orders.far_sender',0)->sum('far')),
-            Stat::make('إجمالي أجور الشحنات TRY  على المرسل', $ordersNum->where('orders.far_sender',1)->sum('far_tr')),
-            Stat::make('إجمالي أجور الشحنات TRY  على المستلم', $ordersNum->where('orders.far_sender',0)->sum('far_tr')),
+            Stat::make('إجمالي أجور الشحنات USD  على المرسل', $ordersNum->where('orders.far_sender',1)->get()->sum('far')),
+            Stat::make('إجمالي أجور الشحنات USD  على المستلم', $ordersNum->where('orders.far_sender',0)->get()->sum('far')),
+            Stat::make('إجمالي أجور الشحنات TRY  على المرسل', $ordersNum->where('orders.far_sender',1)->get()->sum('far_tr')),
+            Stat::make('إجمالي أجور الشحنات TRY  على المستلم', $ordersNum->where('orders.far_sender',0)->get()->sum('far_tr')),
         ];
     }
 }
