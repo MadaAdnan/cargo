fcbe870c9c090aab0f420c3bed7e708617c201fc
sham <albasha.sq@gmail.com>
2025-03-17 10:24:09 +0300

    editProfile

--- routes/api.php
+++ routes/api.php
@@ -25,5 +25,6 @@ Route::prefix('v1')->group(function () {
         Route::post('/profile', [\App\Http\Controllers\Api\AuthController::class, 'profile']);
 
         Route::apiResource('tasks', \App\Http\Controllers\Api\TaskController::class)->only(['index','store','update']);
+        Route::post('order/success',[\App\Http\Controllers\Api\OrderController::class,'SetToSuccess']);
     });
 });
